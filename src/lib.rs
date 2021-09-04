877d7aa462aa49db6d869dbec6f09ac1e855aa56
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-09-04 03:25:16 +0300

    ufmt

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,40 @@ struct TextBox {
     c: u8,
 }
 
+pub struct UfmtBuf<const N: usize> {
+    cursor: u8,
+    buf: [u8; N],
+}
+impl<const N:usize> UfmtBuf<N> {
+    pub fn new() -> Self {
+        Self {
+            cursor: 0,
+            buf: [0u8; N],
+        }
+    }
+    pub fn as_str(&self) -> &str {
+        unsafe{std::str::from_utf8_unchecked(&self.buf[0..(self.cursor as usize)])}
+    }
+}
+impl<const N:usize> ufmt::uWrite for UfmtBuf<N> {
+    type Error = ();
+    fn write_str(&mut self, s: &str) -> Result<(), Self::Error> {
+        if self.cursor as usize + s.len() > N {
+            return Ok(())
+        }
+
+        //self.buf[(self.cursor as usize)..(self.cursor as usize + s.as_bytes().len())].copy_from_slice(s.as_bytes());
+
+        // safety: wasm4's safety standards are lax, so just using it to avoid panic handling code.
+        unsafe {
+            std::ptr::copy_nonoverlapping(s.as_bytes().as_ptr(), self.buf.as_mut_ptr().offset(self.cursor as isize), s.as_bytes().len());
+        }
+
+        self.cursor += s.as_bytes().len() as u8;
+        Ok(())
+    }
+}
+
 impl TextBox {
     const fn new() -> TextBox {
         TextBox {
@@ -131,7 +165,9 @@ impl TextBox {
 
     fn draw(&self, _global_frame: u8) {
         draw_colours(self.c,0,0,0);
-        text("ABCDEFG", 10, 10);
+        let mut buf = UfmtBuf::<11>::new();
+        let _ = ufmt::uwrite!(buf, "QW {}", 33);
+        text(buf.as_str(), 10, 10);
     }
 }
 
