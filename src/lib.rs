e0bd7fefb6d1ad7b82c5efe94601cabfe381db59
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-09-05 05:57:51 +0300

    Camera adjustment

--- src/lib.rs
+++ src/lib.rs
@@ -129,10 +129,10 @@ impl Player {
             let jump_dir = self.jump_dir.as_mut().unwrap();
             if cur & BUTTON_2 != 0 {
                 if cur & BUTTON_LEFT != 0 {
-                    *jump_dir -= 0.02;
+                    *jump_dir -= 0.03;
                 }
                 if cur & BUTTON_RIGHT != 0 {
-                    *jump_dir += 0.02;
+                    *jump_dir += 0.03;
                 }
                 *jump_dir = jump_dir.clamp(-1.0, 1.0);
             } else {
@@ -419,17 +419,88 @@ impl Room {
 
 struct Camera {
     pos: cf32,
+    inertia: cf32,
+    limit: u8,
+    look_down_ctr: u8,
 }
 
 impl Camera {
     const fn new() -> Camera {
         Camera {
             pos: cf32::new(70.0, 70.0),
+            inertia: cf32::new(0.0, 0.0),
+            limit: 0,
+            look_down_ctr: 0,
         }
     }
 
-    fn update(&mut self, p : &Player) {
-        self.pos = p.pos;
+    fn update(&mut self, p : &Player, keys: u8) {
+        let mut futurepos = p.pos + p.vel*0.1;
+        if p.jump_dir.is_some() {
+            futurepos += cf32::new(0.0, -30.0);
+        }
+
+        if !p.grounded && p.jump_dir.is_none() {
+            if keys & BUTTON_UP != 0 {
+                futurepos += cf32::new(0.0, -20.0);
+            }
+            if keys & BUTTON_LEFT != 0 {
+                futurepos += cf32::new(-20.0, 0.0);
+            }
+            if keys & BUTTON_RIGHT != 0 {
+                futurepos += cf32::new(20.0, 0.0);
+            }
+            if keys & BUTTON_DOWN != 0 {
+                futurepos += cf32::new(00.0, 50.0);
+            }
+        } else if p.jump_dir.is_none() {
+            if keys & BUTTON_DOWN != 0 {
+                if self.look_down_ctr > 100 {
+                    if keys & BUTTON_UP != 0 {
+                        futurepos += cf32::new(0.0, -48.0);
+                    } else
+                    if keys & BUTTON_LEFT != 0 {
+                        futurepos += cf32::new(-48.0, 0.0);
+                    } else 
+                    if keys & BUTTON_RIGHT != 0 {
+                        futurepos += cf32::new(48.0, 0.0);
+                    } else {
+                        futurepos += cf32::new(0.0, 48.0);
+                    }
+                } else {
+                    self.look_down_ctr += 1;
+                }
+            } else {
+                self.look_down_ctr = 0;
+            }
+        }
+
+
+        let discr = (self.pos - futurepos).norm();
+        let inertia_norm = self.inertia.norm();
+        if discr < 20.0 || (discr < 28.0 && inertia_norm < 0.1) {
+            if self.limit == 0 {
+                self.inertia = cf32::new(0.0, 0.0);
+            } else {
+                //traceln!("lim {}", self.limit);
+                self.limit -= 1;
+            }
+        } else {
+            let delta = futurepos - self.pos;
+            if inertia_norm > 0.01 {
+                let delta_normalized = delta.unscale(delta.norm());
+                let inertia_normalized = self.inertia.unscale(inertia_norm);
+                if (delta_normalized/inertia_normalized).re < 0.0 {
+                    // angle > 90 deg
+                    self.inertia=cf32::new(0.0,0.0);
+                    return;
+                }
+            }
+            self.inertia = 0.98 * self.inertia + 0.02*delta;
+            self.limit = 5;
+        }
+        self.pos += 0.05 * self.inertia;
+        self.inertia *= 0.99;
     }
 }
 
@@ -472,7 +543,7 @@ impl State {
 
         self.textbox.control(self.prevpad, gamepad);
         
-        self.camera.update(&self.player);
+        self.camera.update(&self.player, gamepad);
         self.room.draw(self.frame, self.player.my_world_coords(), &self.camera);
         self.player.draw(self.frame, gamepad, &self.camera);
         self.textbox.draw(self.frame);
